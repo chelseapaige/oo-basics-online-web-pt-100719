0569ae8666164087151d8e67b53e3a4c53319b4c
Chelsea Hansen <chelseahansen@gmail.com>
2019-10-27 03:57:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,36 +1,12 @@
 class Book
+  attr_accessor :author, :page_count, :genre
   
+  attr_reader :title
   def
   initialize(title)
     @title = title 
-  
-  def title
-      @title
-  end
-  
-  
-  def author=(author)
-    @author = author 
-  end
-  
-  def author
-      @author
-  
-  def page_count=(num)
-      @pages_count = num
-    end
-    
-  def page_count
-      @page_count
-    end
-    
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre 
-  end
+
+
     
   def turn_page 
     puts "Flipping the page...wow, you read fast!"
